bb3ecd6ca4c01702a1fd6d0c2aac7b6dff1872b0
JanneLindberg <janne-lindberg@bahnhof.se>
2015-07-07 00:57:49 +0200

    Refactored and compile lua script to preserve heap space

--- src/ds18b20/init.lua
+++ src/ds18b20/init.lua
@@ -1,6 +1,16 @@
+--
+--
+-- DS18B20's connected to GPIO0
+--
 
-dofile("config.lua")
-dofile("post.lua")
-dofile("send_temp.lua")
+t=require("ds18b20")
+t.setup(3)
 
+require("config")
+require("post")
+require("send_temp")
+
+
+tmr.stop(1)
+tmr.alarm(1,60000,1,send_temp_data)
 
--- src/ds18b20/send_temp.lua
+++ src/ds18b20/send_temp.lua
@@ -1,30 +1,29 @@
 --
+--
+--
+--
 
-t=require("ds18b20")
-t.setup(3)
-
-
-
-function send_data(addr)
-   local json = require "cjson"
+function send_data(t, addr)
+   local json = require("cjson")
    local devid = string.format("%02X-%02X%02X%02X%02X%02X%02X%02X",
                                addr:byte(1),addr:byte(2),addr:byte(3),addr:byte(4),
                                addr:byte(5),addr:byte(6),addr:byte(7),addr:byte(8))
-      
+   
    local msg = {}
-   local p = {}
+   local payload = {}
 
-   p["value"] = tostring(t.read(addr,t.C))
+   payload["value"] = tostring(t.read(addr,t.C))
    msg["dev-id"] = "dev."..devid..".temp"
-   msg["payload"] = p
+   msg["payload"] = payload
    local msg1 = json.encode(msg)
-      
+
    msg=nil
-   p=nil
+   payload=nil
    devid=nil
-      
-   print(msg1)
+
+   print("["..msg1.."]")
    post("["..msg1.."]")
+
    msg1=nil
    json=nil
 end
@@ -34,7 +33,8 @@ function send_temp_data()
    local addrs=t.addrs()
 
    for i=1,#addrs do
-      send_data(addrs[i])
+      send_data(t, addrs[i])
    end
 
+   addrs = nil   
 end
